0e3660f21ef80397d998912715224ec63455bceb
JRSkates <skatesproduction@gmail.com>
2023-04-28 11:59:40 +0100

    Application Class TDD

--- music_library/app.rb
+++ music_library/app.rb
@@ -4,23 +4,55 @@ require_relative 'lib/database_connection'
 require_relative 'lib/artist_repository'
 require_relative 'lib/album_repository'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+class Application
 
-artist_repository = ArtistRepository.new
-album_repository = AlbumRepository.new
+  # The Application class initializer
+  # takes four arguments:
+  #  * The database name to call `DatabaseConnection.connect`
+  #  * the Kernel object as `io` (so we can mock the IO in our tests)
+  #  * the AlbumRepository object (or a double of it)
+  #  * the ArtistRepository object (or a double of it)
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
 
-artist_repository.all.each do |artist|
-  puts "#{artist.id} - #{artist.name} - #{artist.genre}"
+  def run
+    # "Runs" the terminal application
+    # so it can ask the user to enter some input
+    # and then decide to run the appropriate action
+    # or behaviour.
+    @io.puts "Welcome to the music library manager!\n \nWhat would you like to do?\n1 - List all albums\n2 - List all artists\n\nEnter your choice:"
+    result = @io.gets.chomp.to_i
+    case result
+      when 1 
+        @io.puts "\nHere is your list of albums"
+        @album_repository.all.each do |album|
+          @io.puts "* #{album.id} - #{album.title}"
+        end
+      when 2
+        @io.puts "\nHere is your list of artists"
+        @artist_repository.all.each do |artist|
+          @io.puts "* #{artist.id} - #{artist.name} - #{artist.genre}"
+        end
+      end
+    # Use `@io.puts` or `@io.gets` to
+    # write output and ask for user input.
+  end
 end
 
-puts
-
-album_repository.all.each do |album|
-  puts "#{album.id} - #{album.title} - #{album.release_year}"
+# Don't worry too much about this if statement. It is basically saying "only
+# run the following code if this is the main file being run, instead of having
+# been required or loaded by another file.
+# If you want to learn more about __FILE__ and $0, see here: https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Variables_and_Constants#Pre-defined_Variables
+if __FILE__ == $0
+  app = Application.new(
+    'music_library',
+    Kernel,
+    AlbumRepository.new,
+    ArtistRepository.new
+  )
+  app.run
 end
-
-puts 
-
-album_1 = album_repository.find(4)
-puts "#{album_1.id} - #{album_1.title} - #{album_1.release_year}"
